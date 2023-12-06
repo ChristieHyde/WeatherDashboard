e2c6ec211ad2e2f1ee869e1c7ed64813591889fa
ChristieHyde <christieahyde@gmail.com>
2023-12-06 17:38:18 +1100

    implemented search bar in the search form

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,20 @@
-const DEBUG = true;
+const DEBUG = false;
 const apiKey = "52cc4f6275d043cc0b5b26335cce427e";
 
+var searchForm = $("#search-form");
+var searchInput = $("#city-search");
 
 
 
+searchForm.on("submit", (e) => {
+    e.preventDefault();
+    console.log(searchInput.val());
+    // api calls to openweather
+    // display current weather
+    // display forecasts
+
+    // add city to search history
+});
 
 // API Tests
 if (DEBUG) {
