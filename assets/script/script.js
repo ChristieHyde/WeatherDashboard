0ea568bb034815501bf07ac5c4a4139d2ad3a745
ChristieHyde <christieahyde@gmail.com>
2023-12-06 19:32:53 +1100

    Get intended data from API

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,14 +4,31 @@ const apiKey = "52cc4f6275d043cc0b5b26335cce427e";
 var searchForm = $("#search-form");
 var searchInput = $("#city-search");
 
-
-
 searchForm.on("submit", (e) => {
     e.preventDefault();
     console.log(searchInput.val());
-    // api calls to openweather
-    // display current weather
-    // display forecasts
+
+    var searchedCity = searchInput.val();
+    var apiCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}`;
+    var apiForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&appid=${apiKey}&cnt=5`;
+    
+    fetch(apiCurrentUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (currentWeatherRaw) {
+            console.log(currentWeatherRaw);
+            //display
+        })
+    
+    fetch(apiForecastUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (forecastWeatherRaw) {
+            console.log(forecastWeatherRaw);
+            //display
+        })
 
     // add city to search history
 });
