b68c2cd7313e8f0d28d19b4b904cac5f4f82a8a7
ChristieHyde <christieahyde@gmail.com>
2023-12-08 00:59:40 +1100

    Added comments to html and css

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ $(function () {
     const NUM_DAY_FORECAST = 5;
     const API_KEY = "52cc4f6275d043cc0b5b26335cce427e";
 
+    // DOM elements. Elements that should not be visible at page load should be hidden
     var searchFormEl = $("#search-form");
     var searchInputEl = $("#city-search");
     var currentWeatherRootEl = $("#current-weather");
@@ -13,17 +14,20 @@ $(function () {
     currentWeatherRootEl.hide();
     forecastHeadingEl.hide();
 
+    // Initialise search history from local storage
     var searchHistoryList = [];
     if (localStorage.getItem("history")) {
         searchHistoryList = JSON.parse(localStorage.getItem("history"));
     }
     renderSearchHistory();
 
+    // Search for designated city at form submit
     searchFormEl.on("submit", (event) => {
         event.preventDefault();
         searchAndDisplay(searchInputEl.val());
     });
 
+    // Search for designated city at search history click
     searchHistoryRootEl.on("click", (event) => {
         event.preventDefault();
         if(event.target.matches(".search-item")) {
@@ -32,12 +36,14 @@ $(function () {
     });
 
     function searchAndDisplay(searchedCity) {
+        // Empty root elements and show hidden elements
         currentWeatherRootEl.empty();
         forecastRootEl.empty();
         searchHistoryRootEl.empty();
         currentWeatherRootEl.show();
         forecastHeadingEl.show();
 
+        // API request URLs
         var apiCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${API_KEY}&units=metric`;
         var apiForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&appid=${API_KEY}&units=metric`;
         
@@ -48,6 +54,7 @@ $(function () {
             .then(function (currentWeatherRaw) {
                 var currentWeatherProcessed = processWeather(currentWeatherRaw, currentWeatherRaw.timezone);
 
+                // Initialise new DOM elements for current weather card
                 var cityNameEl = $('<h2>');
                 var currentDateEl = $('<p>');
                 var iconImgEl = $('<img>');
@@ -59,6 +66,7 @@ $(function () {
                 var windEl = $('<li>');
                 var humidEl = $('<li>');
 
+                // Populate current weather DOM elements
                 cityNameEl.text(currentWeatherProcessed.city);
                 currentDateEl.text(`Weather at ${currentWeatherProcessed.dateTime}:`);
                 currentTemperatureEl.text(`${currentWeatherProcessed.temperature}°C`);
@@ -70,6 +78,7 @@ $(function () {
                 windEl.text(`Wind speed: ${currentWeatherProcessed.windSpeed}km/h`);
                 humidEl.text(`Humidity: ${currentWeatherProcessed.humidity}%`);
 
+                // Append new DOM elements to existing root elements
                 conditionListEl.append(minEl);
                 conditionListEl.append(maxEl);
                 conditionListEl.append(windEl);
@@ -88,10 +97,14 @@ $(function () {
                 return response.json();
             })
             .then(function (forecastRaw) {
+                // The forecast data contains three-hourly forecasts, so we need to collate them by day
                 var forecastCardList = []
+                
                 for (let i=0; i<forecastRaw.list.length; i++) {
                     var forecastProcessed = processWeather(forecastRaw.list[i], forecastRaw.city.timezone);
                 
+                    // Select the card for the forecast's date
+                    // If none exists, create a new card for that day
                     var currentCard = forecastCardList.find((card) => card.date == forecastProcessed.date);
                     if (!currentCard) {
                         currentCard = {
@@ -104,6 +117,8 @@ $(function () {
                         }
                         forecastCardList.push(currentCard);
                     }
+
+                    // Add the forecast data to the card lists
                     currentCard.summary.push(forecastProcessed.summary);
                     currentCard.iconUrl.push(forecastProcessed.iconUrl);
                     currentCard.temp.push(forecastProcessed.temperature);
@@ -111,11 +126,14 @@ $(function () {
                     currentCard.humid.push(forecastProcessed.humidity);
                 }
 
+                // The result will be a 5 or 6-card long array
+                // If the array has 6 cards, the first is the current day, so we shift it
                 while (forecastCardList.length > NUM_DAY_FORECAST) {
                     forecastCardList.shift();
                 }
 
                 for (let i=0; i<NUM_DAY_FORECAST; i++) {
+                    // Initialise new DOM elements for day's forecast card
                     var forecastDayCardEl = $('<article>')
                     var forecastDateEl = $('<p>');
                     var iconImgEl = $('<img>');
@@ -126,8 +144,11 @@ $(function () {
                     var windEl = $('<li>');
                     var humidEl = $('<li>');
 
+                    // Classes for page style
                     forecastDayCardEl.addClass("forecast-card col-2");
 
+                    // Populate forecast DOM elements
+                    // The most common or maximum element is used to represent the day's conditions
                     forecastDateEl.text(forecastCardList[i].date);
                     conditionSummaryEl.text(mostCommon(forecastCardList[i].summary));
                     iconImgEl.attr("src", mostCommon(forecastCardList[i].iconUrl));
@@ -137,6 +158,7 @@ $(function () {
                     windEl.text(`Max Wind: ${Math.max.apply(null, forecastCardList[i].wind)}km/h`);
                     humidEl.text(`Max Humid: ${Math.max.apply(null, forecastCardList[i].humid)}%`);
         
+                    // Append new DOM elements to existing root elements
                     conditionListEl.append(minEl);
                     conditionListEl.append(maxEl);
                     conditionListEl.append(windEl);
@@ -151,19 +173,20 @@ $(function () {
                 }
             })
 
-        // remove city from search history if it exists in it
+        // Remove city from search history if it exists in it
         var matches = (element) => element == searchedCity;
         var inListIndex = searchHistoryList.findIndex(matches);
         if (inListIndex >= 0) {
             searchHistoryList.splice(inListIndex, 1);
         }
-        // add city to search history
+
+        // Add city to search history and re-render
         searchHistoryList.unshift(searchedCity);
         localStorage.setItem("history", JSON.stringify(searchHistoryList));
-        // re-render search history
         renderSearchHistory();
     }
 
+    // Function to process the raw weather data into desired format
     function processWeather(rawWeather, timezone) {
         var processedWeather = {
             city: "",
@@ -178,6 +201,8 @@ $(function () {
             windSpeed: "",
             humidity: ""
         };
+
+        // Convert raw data
         processedWeather.city = rawWeather.name;
         processedWeather.summary = rawWeather.weather[0].main;
         processedWeather.temperature = rawWeather.main.temp;
@@ -186,10 +211,13 @@ $(function () {
         processedWeather.windSpeed = rawWeather.wind.speed;
         processedWeather.humidity = rawWeather.main.humidity;
 
+        // Get two copies of the weather icon; one larger to display on the current conditions
         var iconCode = rawWeather.weather[0].icon;
         processedWeather.iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
         processedWeather.iconUrlBig = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
+        // To obtain the date in UTC, we add the timezone difference to the timestamp and convert to UTC string
+        // Then slice the string to get the date and time
         var localDate = new Date((rawWeather.dt+timezone)*1000).toUTCString();
         var date = localDate.slice(5, 16);
         var time = localDate.slice(17, 22);
@@ -199,6 +227,7 @@ $(function () {
         return processedWeather;
     }
 
+    // Function to render the search history on the page
     function renderSearchHistory() {
         for (let i = 0; i < searchHistoryList.length; i++) {
             searchItemEl = $('<li>');
@@ -208,7 +237,9 @@ $(function () {
         }
     }
 
+    // Function to find the most common item in array
     function mostCommon(array) {
+        // Create a dictionary object and count items in the array
         var occurrences = {};
         array.forEach((element) => {
             if(occurrences[element]) {
@@ -217,7 +248,11 @@ $(function () {
                 occurrences[element] = 1;
             }
         });
+
+        // Reverse the keys and values in the object
         var occurReverse = Object.fromEntries(Object.entries(occurrences).map(element => element.reverse()));
+
+        // Return the maximum
         return occurReverse[Math.max.apply(null,(Object.values(occurrences)))];
     }
 
